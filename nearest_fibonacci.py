1e60c9f3f73856e98e40cdeab95464f748762c08
DEVITEJAWI1224 <111487462+DEVITEJAWI1224@users.noreply.github.com>
2022-11-05 10:42:18 +0530

    n=int(input()) n1=0 n2=1 s=n1+n2 while(s<=n):     n1=n2     n2=s     s=n1+n2 if abs((s-n)> abs(n2-n)):     print(n2) elif (abs(s-n)==abs(n2-n)):     a=(n2,s)     print(*a)
    else:
        ans=s
        print(ans)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,17 @@
+n=int(input())
+n1=0
+n2=1
+s=n1+n2
+while(s<=n):
+    n1=n2
+    n2=s
+    s=n1+n2
+if abs((s-n)> abs(n2-n)):
+    print(n2)
+elif (abs(s-n)==abs(n2-n)):
+    a=(n2,s)
+    print(*a)
+    
+else:
+    ans=s
+    print(ans)
